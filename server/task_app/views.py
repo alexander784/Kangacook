073ecb688bb1d5279cebc0b4093a15065b2d918d
alexander784 <alexandernyaga1999@gmail.com>
2024-08-20 09:47:26 +0300

    api view for listing tasks

--- server/task_app/views.py
+++ server/task_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .serializers import TaskSerializer
+from .models import Task
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 
 # Create your views here.
+@api_view(['GET','POST'])
+def task_list(request):
+    if request.method == 'GET':
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+    
+
+
