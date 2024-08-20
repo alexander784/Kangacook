f3ccca0b7e58dda3adbe61792766075a25543ca5
alexander784 <alexandernyaga1999@gmail.com>
2024-08-20 17:24:20 +0300

    Create context

---
+++ client/src/context/TaskContext.js
@@ -0,0 +1,35 @@
+import React, { createContext, useReducer } from 'react';
+
+
+export const TaskContext = createContext();
+
+
+const TaskContextProvider = (props) => {
+    const [tasks, dispatch] = useReducer(taskReducer,[]);
+    useEffect(() => {
+        fetch('http://127.0.0.1:8000/tasks_app')
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Failed to fetch tasks');
+                }
+            })
+            .then((data) => {
+                dispatch({ type: 'SET_TASKS', tasks: data });
+            })
+            .catch((err) => {
+                console.log('Error fetching tasks:', err);
+            });
+    }, []);
+
+    return (
+        <TaskContext.Provider value={{ tasks, dispatch }}>
+            
+            {props.children}
+        </TaskContext.Provider>
+    );
+};
+
+export default TaskContextProvider;
+
