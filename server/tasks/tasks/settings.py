62da55f1497975467110ff429f60e79d955df5c2
alexander784 <alexandernyaga1999@gmail.com>
2024-08-20 09:55:12 +0300

    update installed apps

--- server/tasks/tasks/settings.py
+++ server/tasks/tasks/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'task_app.apps.TaskAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
