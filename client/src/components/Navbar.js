a07410aaf5ddec6c27fa2a15b8d8b32e63b77519
alexander784 <alexandernyaga1999@gmail.com>
2024-08-20 15:25:47 +0300

    Structure the NavBar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-   <>
-   </>
+    <nav className='bg-green-400 p-4'>
+        <div className='container mx-auto flex items-center'>
+            <div className='flex-shrink-0'>
+                <img src='' alt='' className='h-8 w-8'/>
+            </div>
+            <div className='ml-4 text-black text-xl font-semibold'>
+                Task Manager
+            </div>
+        </div>
+
+    </nav>
   )
 }
 
